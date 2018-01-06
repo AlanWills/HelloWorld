49ce814dd54e115941c8b23b8ef0947d2f8f2353
Alan Wills <alawills@googlemail.com>
2018-01-06 15:47:19 +0000

    Finished options screen

---
+++ HelloWorldGame/Source/DataConverters/UI/OptionsScreenUIManagerDataConverter.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#include "DataConverters/UI/OptionsScreenUIManagerDataConverter.h"
+
+
+namespace HW
+{
+  REGISTER_COMPONENT_DATA_CONVERTER(OptionsScreenUIManagerDataConverter)
+}
